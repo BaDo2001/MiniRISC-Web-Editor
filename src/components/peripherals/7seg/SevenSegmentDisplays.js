221d13470f67ab054c0b0eb523b11b7fe9defd32
BaDo2001 <balint.dolla@gmail.com>
2020-01-15 19:59:24 +0100

    7seg draggable

--- src/components/peripherals/7seg/SevenSegmentDisplays.js
+++ src/components/peripherals/7seg/SevenSegmentDisplays.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Draggable from "react-draggable";
+
 import SevenSeg from "./7seg";
 import processor from "../../../compiler/processor";
 
@@ -19,12 +21,14 @@ const SevenSegmentDisplays = () => {
 	setTimeout(testF, 100);
 
 	return (
-		<div>
-			<SevenSeg x={900} y={20} byte={processor.memory[144]} />
-			<SevenSeg x={1050} y={20} byte={processor.memory[145]} />
-			<SevenSeg x={1200} y={20} byte={processor.memory[146]} />
-			<SevenSeg x={1350} y={20} byte={processor.memory[147]} />
-		</div>
+		<Draggable>
+			<div>
+				<SevenSeg x={900} y={20} byte={processor.memory[144]} />
+				<SevenSeg x={1050} y={20} byte={processor.memory[145]} />
+				<SevenSeg x={1200} y={20} byte={processor.memory[146]} />
+				<SevenSeg x={1350} y={20} byte={processor.memory[147]} />
+			</div>
+		</Draggable>
 	);
 };
 
