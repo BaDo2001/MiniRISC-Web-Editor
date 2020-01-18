efd49329db114baec99a5e2a06de560fe0990811
BaDo2001 <balint.dolla@gmail.com>
2020-01-18 14:52:27 +0100

    Removed worker

--- src/components/layout/RunButton.js
+++ src/components/layout/RunButton.js
@@ -1,43 +1,12 @@
 import React, { useContext } from "react";
 import EditorContext from "../../context/editor/editorContext";
-import processor from "../../compiler/processor";
-
-import WebWorker from "../../worker/WebWorker";
-import worker from "../../worker/app.worker.js";
 
 const RunButton = () => {
 	const { code } = useContext(EditorContext);
 
 	return (
 		<button
-			onClick={() => {
-				let runner = new WebWorker(worker);
-				let obj = JSON.parse(JSON.stringify(processor));
-
-				runner.postMessage({
-					type: "RUN_CODE",
-					processor: obj,
-					code: code
-				});
-
-				const stopWorker = () => {
-					runner.terminate();
-					runner = undefined;
-				};
-
-				runner.onmessage = (event) => {
-					switch (event.data.type) {
-						case "TERMINATE":
-							stopWorker();
-							break;
-						case "ERROR":
-							console.log("ERROR IN WEB WORKER");
-							break;
-						default:
-							console.log("UNKNWON EVENT TYPE");
-					}
-				};
-			}}
+			onClick={() => {}}
 			style={{
 				position: "absolute",
 				left: window.innerWidth * 0.5 + 10,
